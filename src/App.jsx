e116e6f8fa3c7a30544087829e4b7c404409da0e
Preethi <writetopreethir@gmail.com>
2023-12-21 22:51:46 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import ContactList from './components/ContactList'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <ContactList />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ContactList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ContactList = () => {
+  return (
+    <div>
+      <h1>This is contact list component!</h1>
+    </div>
+  )
+}
+
+export default ContactList;
